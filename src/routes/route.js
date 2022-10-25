0ce9ae0cd8a232265ff18aab773ef2a3cc7afade
chetan6129 <vermachetan556@gmail.com>
2022-10-25 14:56:52 +0530

    book

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,8 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createnewbook", UserController.createnewbook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getnewbook", UserController.getnewbook)
 
 module.exports = router;
