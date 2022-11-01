bd15feceb1ed91b112f38e5b7a6a61bbc5e9b0c3
chetan6129 <vermachetan556@gmail.com>
2022-11-01 21:27:39 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+var moment = require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://chetan:test@cluster1.ia9rznm.mongodb.net/newarjun", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -16,10 +17,14 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        console.log(moment().format('YYYY MM Do, h:mm:ss a')+" , "+req.ip+"  ,  "+req.url);
+     
+      
         next();
-  }
-  );
+    })
+  ;
+
+
 
 app.use('/', route);
 
