00cde0e28547a36b095aa43fb8a1ed012e106b54
chetan6129 <vermachetan556@gmail.com>
2022-11-05 11:40:27 +0530

    assaiment is done

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,48 +1,37 @@
+const { isValidObjectId } = require("mongoose")
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
-    next()
-}
-
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
-
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
-
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
 
 const myMiddleware = function(req, res, next){
     req.month = "November"
     console.log('I am inside a middleware!')
     next()
 }
-
-const myOtherMiddleware = function(req, res, next){
-    // Setting an attribute 'wantsJson' in request
-    // The header value comparison is done once and
-    // the result can be used directly wherever required.
-    let acceptHeaderValue = req.headers["accept"]
-
-    if(acceptHeaderValue == "application/json") {
-        req.wantsJson = true
-    } else {
-        req.wantsJson = false
+//ans
+const checkpoint=function(req,res,next){
+    let check= req.headers["isfreeappuser"]
+    if(!check){
+         return res.send("error")
+    
+    }else{
+        next()
     }
-    next()
 }
 
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+const vId=function(req,res,next){
+     const data=req.body
+     const uid=data.userId
+     const pid=data.productId
+     const uidisvaild=isValidObjectId(uid)
+     const pidisvaild=isValidObjectId(pid)
+     if(!uidisvaild && !pidisvaild){
+       return res.send("check your id's")
+     }else{
+        next()
+     }
+}
+
+
 module.exports.myMiddleware = myMiddleware
-module.exports.myOtherMiddleware = myOtherMiddleware
+module.exports.vId=vId
+
+module.exports.checkpoint=checkpoint
