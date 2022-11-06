9ddf65be8e798af43d90459548d589915d674209
chetan6129 <vermachetan556@gmail.com>
2022-11-06 12:08:49 +0530

    jwt assaiment

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,14 +5,17 @@ const userController= require("../controllers/userController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
-router.post("/users", userController.createUser  )
-
+//1
+router.post("/sign", userController.createUser  )
+//2
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+//3
+router.get("/login/:userId", userController.getUserData)
 
+//4
 router.put("/users/:userId", userController.updateUser)
+//5
+router.delete("/users/:userId", userController.no)
 
 module.exports = router;
